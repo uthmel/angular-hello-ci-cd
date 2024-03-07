f75bad9f0ff5a9d85aa5aceefd5524e6b1cdb151
Lewis <ljoncb@gmail.com>
2024-03-07 11:34:18 +0000

    feat: basic page completed

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,26 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+
+declare var window: any;
 
 @Component({
   selector: 'app-root',
   standalone: true,
   imports: [RouterOutlet],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  styleUrl: './app.component.css',
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'hello-ci-cd';
+
+  domain: string = '';
+
+  ngOnInit() {
+    const parsedUrl = new URL(window.location.href);
+    const baseUrl = parsedUrl.origin;
+    this.domain = baseUrl
+      .replace('http://', '')
+      .replace('https://', '')
+      .replace(':4200', '');
+  }
 }
